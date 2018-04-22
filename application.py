ec3805f4dee9d51b05644306b434b2349ac29813
Naxaes <ted.kleinman@hotmail.com>
2018-04-22 05:04:45 +0200

    Update application.py
    Refactored the code to remove duplications (along with some general clean-up).

--- application.py
+++ application.py
@@ -1,9 +1,8 @@
 """
 It's often very useful to look at other people's code to learn new and effective ways to program!
 
-But not here. This file contains some pretty wacky stuff, which shouldn't be used in
-real production code. It works for this small case, but doesn't scale well, is hard to debug, and generally
-is not optimal.
+But not here. This file contains things that shouldn't be used in real production code. It works for this small case,
+but doesn't scale well, is hard to debug, and generally is not optimal.
 """
 
 from random import choice
@@ -11,34 +10,35 @@ from random import choice
 from flask import Flask, render_template, redirect, url_for, request, flash, session
 from wtforms import Form, StringField, PasswordField, TextAreaField, validators
 
-from exercise_queries import (
+from solution_queries import (
     get_newest_tweets, search_for_tweets, get_user, create_user, validate_login, post_tweet,
     validate_and_perform_user_changes, get_user_by_ID, get_user_followers, get_followers_tweets, add_follower,
     remove_follower, remove_tweet
 )
 
-app = Flask(__name__)
-messages = [
-    "Make sure to get the latest from you!",
-    "There are exactly the same tweets here as last time. Predictability and consistency is gold!",
-    "There are only quality tweets when no one else can post.",
-]
+
+class NotLoggedInException(Exception):
+    pass
+
+
+class DatabaseException(Exception):
+    pass
 
 
 class RegisterForm(Form):
     username = StringField(
         'Username ',
-        validators=[validators.input_required(message='Must provide a username!'), validators.length(min=4, max=100)]
+        validators=[validators.input_required(message='Must provide a username!'), validators.length(min=1, max=144)]
     )
     email = StringField(
-        'Email ', validators=[validators.input_required(message='Must provide an email!')]
+        'Email ', validators=[validators.input_required(message='Must provide an email!'), validators.length(min=1, max=144)]
     )
     age = StringField(
         'Age', validators=[]
     )
     password = PasswordField(
         'Password',
-        validators=[validators.input_required(message='Must provide a password!')]
+        validators=[validators.input_required(message='Must provide a password!'), validators.length(min=1, max=144)]
     )
     confirm = PasswordField(
         'Confirm', validators=[
@@ -51,21 +51,21 @@ class RegisterForm(Form):
 class ChangeInfoForm(Form):
     username = StringField(
         'Change username ',
-        validators=[validators.optional(), validators.length(min=4, max=100)]
+        validators=[validators.length(min=1, max=144)]
     )
     email = StringField(
-        'Change email ', validators=[]
+        'Change email ', validators=[validators.length(max=144)]
     )
     age = StringField(
         'Change age', validators=[]
     )
     password = PasswordField(
         'Change password',
-        validators=[]
+        validators=[validators.length(max=144)]
     )
     confirm = PasswordField(
         'Confirm with current password', validators=[
-            validators.input_required(message='Must approve changes with your password!'),
+            validators.input_required(message='Must approve changes with your password!')
         ]
     )
 
@@ -92,142 +92,45 @@ class TweetForm(Form):
     )
 
 
+app = Flask(__name__)
+messages = [
+    "Make sure to get the latest from you!",
+    "There are exactly the same tweets here as last time. Predictability and consistency is gold!",
+    "There are only quality tweets when no one else can post.",
+]
+default_message = 'A place where you can send tweets by yourself, to yourself!'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = SearchForm(request.form)
 
     if request.method == 'POST':
-
-        follow   = request.form.get('follow')
-        unfollow = request.form.get('unfollow')
-        delete   = request.form.get('delete')
-
-        if follow:
-            if session.get('logged_in'):
-                if not add_follower(session['user'][0], follow):
-                    flash('Something went wrong!', 'danger')
-            else:
-                flash('You must be logged in to follow a user.', 'danger')
-                return redirect(url_for('login'))
-        elif unfollow:
-            if session.get('logged_in'):
-                if not remove_follower(session['user'][0], unfollow):
-                    flash('Something went wrong!', 'danger')
-            else:
-                flash('You must be logged in to unfollow a user.', 'danger')
-                return redirect(url_for('login'))
-        elif delete:
-            if not remove_tweet(delete):
-                flash('Something went wrong!', 'danger')
-            flash('Tweet deleted.', 'success')
-
-        elif form.validate():
-            return redirect(url_for('tweets_search', search=form.username.data))
+        try:
+            completed = dispatch_request_form(follow=follow_user, unfollow=unfollow_user, delete=delete_tweet)
+            if not completed and form.validate():
+                return redirect(url_for('tweets_search', search=form.username.data))
+        except (NotLoggedInException, DatabaseException):
+            return redirect(url_for('login'))
 
     if session.get('logged_in'):
-        # tweets = get_user_tweets(session['user'][0])
-        tweets = get_followers_tweets(session['user'][0])
-        followers = get_user_followers(session['user'][0])
+        user_id = session['user'][0]
+        tweets  = get_followers_tweets(user_id)
+        followers = get_user_followers(user_id)
         return render_template('home.html', form=form, tweets=chunks(tweets, 3), message=choice(messages), followers=followers)
     else:
-        tweets = get_newest_tweets(9)
-        return render_template('home.html', form=form, tweets=chunks(tweets, 3), message='A place where you can send tweets by yourself, to yourself!', followers=[])
-
+        tweets = get_newest_tweets(18)
+        return render_template('home.html', form=form, tweets=chunks(tweets, 3), message=default_message, followers=[])
 
 
 @app.route('/tweets/search=<string:search>', methods=['GET', 'POST'])
 def tweets_search(search):
-    form = TweetForm(request.form)
-
-    if request.method == 'POST':
-
-        follow = request.form.get('follow')
-        unfollow = request.form.get('unfollow')
-        delete = request.form.get('delete')
-
-        if follow:
-            if session.get('logged_in'):
-                if not add_follower(session['user'][0], follow):
-                    flash('Something went wrong!', 'danger')
-            else:
-                flash('You must be logged in to follow a user.', 'danger')
-                return redirect(url_for('login'))
-        elif unfollow:
-            if session.get('logged_in'):
-                if not remove_follower(session['user'][0], unfollow):
-                    flash('Something went wrong!', 'danger')
-            else:
-                flash('You must be logged in to unfollow a user.', 'danger')
-                return redirect(url_for('login'))
-        elif delete:
-            if not remove_tweet(delete):
-                flash('Something went wrong!', 'danger')
-            flash('Tweet deleted.', 'success')
-        elif form.validate():
-            if session.get('logged_in'):
-                if post_tweet(session['user'][0], form.content.data):
-                    flash('Your tweet has been posted!', 'success')
-                    return redirect(url_for('tweets_'))
-                else:
-                    flash('Something went wrong!', 'danger')
-            else:
-                flash('You must be logged in to post a tweet.', 'danger')
-                return redirect(url_for('login'))
-
-    tweets = search_for_tweets(search)
-    if session.get('logged_in'):
-        followers = get_user_followers(session['user'][0])
-    else:
-        followers = []
-    return render_template('tweets.html', form=form, tweets=chunks(tweets, 3), followers=followers)
+    return render_tweet_page(search)
 
 
 @app.route('/tweets', methods=['GET', 'POST'])
-def tweets_():
-    form = TweetForm(request.form)
-
-    if request.method == 'POST':
-
-        follow = request.form.get('follow')
-        unfollow = request.form.get('unfollow')
-        delete = request.form.get('delete')
-
-        if follow:
-            if session.get('logged_in'):
-                if not add_follower(session['user'][0], follow):
-                    flash('Something went wrong!', 'danger')
-            else:
-                flash('You must be logged in to follow a user.', 'danger')
-                return redirect(url_for('login'))
-        elif unfollow:
-            if session.get('logged_in'):
-                if not remove_follower(session['user'][0], unfollow):
-                    flash('Something went wrong!', 'danger')
-            else:
-                flash('You must be logged in to unfollow a user.', 'danger')
-                return redirect(url_for('login'))
-        elif delete:
-            if not remove_tweet(delete):
-                flash('Something went wrong!', 'danger')
-            flash('Tweet deleted.', 'success')
-        elif form.validate():
-            if session.get('logged_in'):
-                if post_tweet(session['user'][0], form.content.data):
-                    flash('Your tweet has been posted!', 'success')
-                    return redirect(url_for('tweets_'))
-                else:
-                    flash('Something went wrong!', 'danger')
-            else:
-                flash('You must be logged in to post a tweet.', 'danger')
-                return redirect(url_for('login'))
-
-    tweets = get_newest_tweets(9)
-    if session.get('logged_in'):
-        followers = get_user_followers(session['user'][0])
-    else:
-        followers = []
-    return render_template('tweets.html', form=form, tweets=chunks(tweets, 3), followers=followers)
+def default_tweet_page():
+    return render_tweet_page()
 
 
 @app.route('/contact')
@@ -237,7 +140,6 @@ def contact():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-
     form = RegisterForm(request.form)
 
     if request.method == 'POST' and form.validate():
@@ -252,7 +154,6 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
     form = LoginForm(request.form)
 
     if request.method == 'POST' and form.validate():
@@ -269,7 +170,7 @@ def login():
         elif result == -1:
             flash('Invalid email.', 'danger')
         else:
-            pass
+            raise DatabaseException('Invalid result from "validate_login".')
 
     return render_template('login.html', form=form)
 
@@ -284,26 +185,100 @@ def logout():
 
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
-
-    if not session.get('logged_in', False):
+    if not session.get('logged_in'):
         flash('No!', 'danger')
         return redirect(url_for('home'))
 
     form = ChangeInfoForm(request.form)
 
     if request.method == 'POST' and form.validate():
-
+        userID = session['user'][0]
         result = validate_and_perform_user_changes(
-            session['user'][0], form.confirm.data, form.username.data, form.email.data, form.age.data, form.password.data
+            userID, form.confirm.data, form.username.data, form.email.data, form.age.data, form.password.data
         )
 
         if result:
-            session['user'] = get_user_by_ID(session['user'][0])
-            flash('Changed made!', 'success')
+            session['user'] = get_user_by_ID(userID)
+            flash('Change made!', 'success')
         else:
             flash('Wrong password!', 'danger')
 
     return render_template('settings.html', form=form)
+
+
+def render_tweet_page(search=None):
+    form = TweetForm(request.form)
+
+    if request.method == 'POST':
+
+        try:
+            completed = dispatch_request_form(follow=follow_user, unfollow=unfollow_user, delete=delete_tweet)
+            if not completed:
+                validate_tweet_form(form)
+        except (NotLoggedInException, DatabaseException):
+            return redirect(url_for('login'))
+
+    if search:
+        tweets = search_for_tweets(search)
+    else:
+        tweets = get_newest_tweets(18)
+
+    if session.get('logged_in'):
+        userID = session['user'][0]
+        followers = get_user_followers(userID)
+        return render_template('tweets.html', form=form, tweets=chunks(tweets, 3), followers=followers)
+    else:
+        return render_template('tweets.html', form=form, tweets=chunks(tweets, 3), followers=[])
+
+
+def follow_user(user_to_follow):
+    if session.get('logged_in'):
+        userID = session['user'][0]
+        if not add_follower(userID, user_to_follow):
+            flash('Something went wrong!', 'danger')
+    else:
+        flash('You must be logged in to follow a user.', 'danger')
+        raise NotLoggedInException()
+
+
+def unfollow_user(user_to_unfollow):
+    if session.get('logged_in'):
+        userID = session['user'][0]
+        if not remove_follower(userID, user_to_unfollow):
+            flash('Something went wrong!', 'danger')
+    else:
+        flash('You must be logged in to unfollow a user.', 'danger')
+        raise NotLoggedInException()
+
+
+def delete_tweet(tweet):
+    if not remove_tweet(tweet):
+        flash('Something went wrong!', 'danger')
+        raise DatabaseException()
+    flash('Tweet deleted.', 'success')
+
+
+def validate_tweet_form(form):
+    if form.validate():
+        if session.get('logged_in'):
+            userID = session['user'][0]
+            if post_tweet(userID, form.content.data):
+                flash('Your tweet has been posted!', 'success')
+            else:
+                flash('Something went wrong!', 'danger')
+        else:
+            flash('You must be logged in to post a tweet.', 'danger')
+            raise NotLoggedInException()
+
+
+def dispatch_request_form(**kwargs):
+    completed = False
+    for word, func in kwargs.items():
+        argument = request.form.get(word)
+        if argument:
+            func(argument)
+            completed = True
+    return completed
 
 
 def chunks(sequence, n):
