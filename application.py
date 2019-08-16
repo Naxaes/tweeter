dd779a33d642511d5206f43f494f7783d2bafa76
Ted Klein Bergman <ted.kleinman@hotmail.com>
2019-08-17 00:50:49 +0200

    Refactored all forms to their own file

--- application.py
+++ application.py
@@ -3,18 +3,24 @@ It's often very useful to look at other people's code to learn new and effective
 
 But not here. This file contains things that shouldn't be used in real production code. It works for this small case,
 but doesn't scale well, is hard to debug, and generally is not optimal.
-"""
 
+For one, it sets up a debug session.
+"""
+# Built-in python packages
 from random import choice
 
+# Third-party python packages
 from flask import Flask, render_template, redirect, url_for, request, flash, session
-from wtforms import Form, StringField, PasswordField, TextAreaField, validators
 
+# Project python packages
+# This import can be switched between 'exercise_queries' and 'solution_queries.py' in order to test your implementation
+# against the expected implementation.
 from exercise_queries import (
     get_newest_tweets, search_for_tweets, get_user, create_user, validate_login, post_tweet,
     validate_and_perform_user_changes, get_user_by_ID, get_user_followers, get_followers_tweets, add_follower,
     remove_follower, remove_tweet
 )
+from views import SearchForm, LoginForm, RegisterForm, TweetForm, ChangeInfoForm
 
 
 class NotLoggedInException(Exception):
@@ -23,73 +29,6 @@ class NotLoggedInException(Exception):
 
 class DatabaseException(Exception):
     pass
-
-
-class RegisterForm(Form):
-    username = StringField(
-        'Username ',
-        validators=[validators.input_required(message='Must provide a username!'), validators.length(min=1, max=144)]
-    )
-    email = StringField(
-        'Email ', validators=[validators.input_required(message='Must provide an email!'), validators.length(min=1, max=144)]
-    )
-    age = StringField(
-        'Age', validators=[]
-    )
-    password = PasswordField(
-        'Password',
-        validators=[validators.input_required(message='Must provide a password!'), validators.length(min=1, max=144)]
-    )
-    confirm = PasswordField(
-        'Confirm', validators=[
-            validators.input_required(message='Must confirm password!'),
-            validators.equal_to('password', message="Password didn't match!")
-        ]
-    )
-
-
-class ChangeInfoForm(Form):
-    username = StringField(
-        'Change username ',
-        validators=[validators.length(min=1, max=144)]
-    )
-    email = StringField(
-        'Change email ', validators=[validators.length(max=144)]
-    )
-    age = StringField(
-        'Change age', validators=[]
-    )
-    password = PasswordField(
-        'Change password',
-        validators=[validators.length(max=144)]
-    )
-    confirm = PasswordField(
-        'Confirm with current password', validators=[
-            validators.input_required(message='Must approve changes with your password!')
-        ]
-    )
-
-
-class LoginForm(Form):
-    email = StringField(
-        'Email ', validators=[validators.input_required(message='Must provide username')]
-    )
-    password = PasswordField(
-        'Password', validators=[validators.input_required(message='Must provide password')]
-    )
-
-
-class SearchForm(Form):
-    username = StringField('', validators=[validators.input_required(message='Cannot search for nothing...')])
-
-
-class TweetForm(Form):
-    content = TextAreaField(
-        '', validators=[
-            validators.input_required(message='Cannot post emtpy tweets...'),
-            validators.length(min=1, max=144)
-        ]
-    )
 
 
 app = Flask(__name__)
---
+++ views.py
@@ -0,0 +1,68 @@
+from wtforms import Form, StringField, PasswordField, TextAreaField, validators
+
+
+class RegisterForm(Form):
+    username = StringField(
+        'Username ',
+        validators=[validators.input_required(message='Must provide a username!'), validators.length(min=1, max=144)]
+    )
+    email = StringField(
+        'Email ', validators=[validators.input_required(message='Must provide an email!'), validators.length(min=1, max=144)]
+    )
+    age = StringField(
+        'Age', validators=[]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[validators.input_required(message='Must provide a password!'), validators.length(min=1, max=144)]
+    )
+    confirm = PasswordField(
+        'Confirm', validators=[
+            validators.input_required(message='Must confirm password!'),
+            validators.equal_to('password', message="Password didn't match!")
+        ]
+    )
+
+
+class ChangeInfoForm(Form):
+    username = StringField(
+        'Change username ',
+        validators=[validators.length(min=1, max=144)]
+    )
+    email = StringField(
+        'Change email ', validators=[validators.length(max=144)]
+    )
+    age = StringField(
+        'Change age', validators=[]
+    )
+    password = PasswordField(
+        'Change password',
+        validators=[validators.length(max=144)]
+    )
+    confirm = PasswordField(
+        'Confirm with current password', validators=[
+            validators.input_required(message='Must approve changes with your password!')
+        ]
+    )
+
+
+class LoginForm(Form):
+    email = StringField(
+        'Email ', validators=[validators.input_required(message='Must provide username')]
+    )
+    password = PasswordField(
+        'Password', validators=[validators.input_required(message='Must provide password')]
+    )
+
+
+class SearchForm(Form):
+    username = StringField('', validators=[validators.input_required(message='Cannot search for nothing...')])
+
+
+class TweetForm(Form):
+    content = TextAreaField(
+        '', validators=[
+            validators.input_required(message='Cannot post emtpy tweets...'),
+            validators.length(min=1, max=144)
+        ]
+    )
